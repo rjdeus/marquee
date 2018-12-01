2bd929d03a65294b97d99a0b3c66a1eaef02b64c
MHN-II <nimrim@mcmaster.ca>
2018-12-01 04:14:33 -0500

    update

--- main.py
+++ main.py
@@ -11,16 +11,10 @@ def main(gsidval):
     company_id = ticker(gsidval, ticker_url) # company_id = [company, gsid, ticker]
     #data = gsid(gsidval, gsid_url) # data = [growthScore, financialReturnsScore, multipleScore, integratedScore ]
     gsid_dataframe = gsid(gsidval, gsid_url) # datafram with gsid data (4 things, + date)
-<<<<<<< HEAD
-    stprice = price(company_id[2])
-    company_id.append(stprice)
     print(company_id, stprice)
     print(gsid_dataframe)
+    return company_id, gsid_dataframe   
 
-main()    
-=======
-    #print(gsid_dataframe)
-    #print(company_id)
-    return company_id, gsid_dataframe    
+
 main("11308")
->>>>>>> 58a0b5791b3b2dd36300f2de89b99894bbe233d5
+
