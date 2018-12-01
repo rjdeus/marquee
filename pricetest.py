ed82f5575e3478e9b3514fe8d7fc23db6b7c62b5
Gabriel <yug13@mcmaster.ca>
2018-12-01 04:34:29 -0500

    xx

--- pricetest.py
+++ pricetest.py
@@ -1,12 +1,8 @@
 from datetime import datetime
 from iexfinance.stocks import get_historical_data
 
-def historicalPrice(sYear,sMonth,sDay,eYear,eMonth,eDay)
-	start = datetime(sYear, sMonth, sDay)
-	end = datetime(eYear, eMonth, eDay)
-
-
-	df = get_historical_data("KO", start, end, output_format='pandas')
+def historicalPrice(dStart, dEnd):
+	df = get_historical_data("KO", dStart, dEnd, output_format='pandas')
 	df = df.drop('open', axis=1)
 	df = df.drop('high', axis=1)
 	df = df.drop('low', axis=1)
