dc491ff12969c14697a553c73694d415834a9e51
Gabriel <yug13@mcmaster.ca>
2018-12-01 11:19:22 -0500

    tables.py

--- save_company_data.py
+++ save_company_data.py
@@ -29,32 +29,3 @@ def save():
         merged = price_df.join(gsid_df)
         merged.to_csv("data/"+myGSID+".csv", encoding='utf-8', index=False)
 
-    ###attributes = ['growthScore','financialReturnsScore','multipleScore','integratedScore']
-
-    #### Tables
-        
-    '''
-    plt.rcParams["figure.figsize"] = [plt.rcParams["figure.figsize"][0] *2, plt.rcParams["figure.figsize"][1]]
-    merged.plot(x = 'date', y = attributes)
-    plt.legend(bbox_to_anchor=(0.7, 0.7), loc=2,
-               ncol=2, mode="expand", borderaxespad=0.)
-    plt.suptitle(company_id[0] + ": Fundamental Attributes vs Time")
-    plt.ylabel("Attribute Value")
-    plt.xlabel("Date")
-    #plt.savefig('attributetime.png')
-
-    merged.plot(x = 'date', y = 'close')
-    plt.title(company_id[0] + ": Close Price vs Time")
-    leg = plt.legend( loc = 'best')
-    plt.ylabel("Cost ($ US)")
-    plt.xlabel("Date")
-    #plt.savefig('costtime.png')
-    plt.show()
-    '''
-
-    '''
-    plt.plot(merged['date'], merged['growthScore'])
-    plt.title(company_id[0] + " growthScore over Time")
-    plt.xticks([merged['date'].iloc[0],merged['date'].iloc[len(merged['date'])//2],merged['date'].iloc[-1]])
-    plt.show()
-    '''
