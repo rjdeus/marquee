24ea63f312c8b89ae8055a68220c7c792c0e2aa3
rjdeus <41559152+rjdeus@users.noreply.github.com>
2018-12-01 20:49:15 -0500

    updated linear regression model

--- regression.py
+++ regression.py
@@ -1,18 +1,23 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import datetime as dt
 
 dataset = pd.read_csv('data/10516.csv')
 print(dataset.shape)
-#print(dataset.head())
-#print(dataset.describe())
+
 '''
+#for multiple regression model
 X = dataset[['growthScore','financialReturnsScore','multipleScore','integratedScore']]
 y = dataset['close']
 '''
-X=dataset[['growthScore']].values
-y=dataset['close'].values
 
+dataset['date']=pd.to_datetime(dataset['date'])
+dataset['date']=dataset['date'].map(dt.datetime.toordinal)
+
+#X=dataset[['growthScore']].values
+X=dataset[['date']]
+y=np.asarray(dataset['close'])
 
 #80/20 split for training and test set
 from sklearn.model_selection import train_test_split
@@ -26,15 +31,19 @@ print(y_train)
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
+
 '''
+#coefficient for multiple regression
 coeff_df = pd.DataFrame(regressor.coef_, X.columns, columns=['Coefficient'])
 print('\n')
 print(coeff_df)
 '''
+
+
 #making predictions
 y_pred = regressor.predict(X_test)
 df = pd.DataFrame({'Actual' : y_test, 'Predicted' : y_pred})
-df.to_csv('regressionmodel.csv',encoding='utf-8',index=False)
+#df.to_csv('regressionmodel.csv',encoding='utf-8',index=False)
 #df.sort_index(inplace=True)
 print('\n')
 print('\n')
@@ -47,17 +56,6 @@ print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
 print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
 
 
-'''
-dataset.plot(x='Hours', y='Scores', style='o')  
-plt.title('Hours vs Percentage')  
-plt.xlabel('Hours Studied')  
-plt.ylabel('Percentage Score')  
-plt.show()  
-'''
-
 print('\n')
-print(y_pred)
-print('\n')
-print(X_test)
 plt.scatter(X_test, y_pred,  color='black')
 plt.show()
